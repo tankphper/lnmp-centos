b2118833364d2d4b6b1c2cc8849506906311edd3
Tank <tankphper@gmail.com>
2017-08-29 14:53:22 +0800

    fix

--- php.sh
+++ php.sh
@@ -19,7 +19,7 @@ MCRYPT_DOWN="https://downloads.sourceforge.net/project/mcrypt/Libmcrypt/2.5.8/li
 MCRYPT_SRC="libmcrypt-2.5.8"
 MCRYPT_LOCK="$LOCK_DIR/mcrypt.lock"
 # php7 source
-PHP_DOWN="http://cn2.php.net/distributions/php-7.1.8.tar.gz"
+PHP_DOWN="http://hk1.php.net/distributions/php-7.1.8.tar.gz"
 PHP_SRC="php-7.1.8"
 PHP_DIR="$PHP_SRC"
 PHP_LOCK="$LOCK_DIR/php.lock"
