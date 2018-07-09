4fb922e9ba2112ac3c604e9340679fc8d60699c7
Tank <tankphper@gmail.com>
2018-07-09 16:36:10 +0800

    add php extension install script

---
+++ php-extension.sh
@@ -0,0 +1,20 @@
+. ./common.sh
+
+SRC_DIR="$ROOT/src"
+LOCK_DIR="$ROOT/lock"
+SRC_SUFFIX=".tar.gz"
+
+function add_redis {
+    PHPREDIS_VER = $2
+    cd $SRC_DIR
+    wget https://github.com/phpredis/phpredis/archive/$PHPREDIS_VER.tar.gz -O phpredis.tar.gz
+    mkdir phpredis
+    tar -zxvf phpredis.tar.gz -C phpredis --strip-components=1
+    cd phpredis
+    phpize
+    ./configure --with-php-config=/www/server/php/bin/php-config
+    make
+    make install
+}
+
+add_$1
