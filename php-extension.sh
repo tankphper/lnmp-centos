c4dbad29f4224d7fc9749f134adba8226f934d67
Tank <tankphper@gmail.com>
2019-01-02 17:00:00 +0800

    add mcrypt extension

--- php-extension.sh
+++ php-extension.sh
@@ -4,6 +4,15 @@ SRC_DIR="$ROOT/src"
 LOCK_DIR="$ROOT/lock"
 SRC_SUFFIX=".tar.gz"
 PHPREDIS_VER=$2
+PHP_VER=$(php --version | grep "^PHP" | awk '{print $2}')
+PHP_API_VER=$(phpize --version | grep "PHP Api Version*" | awk '{print $NF}')
+
+echo $PHP_VER
+echo $PHP_API_VER
+
+function echo_ini {
+    echo "extension=/www/server/php-$PHP_VER/lib/php/extensions/no-debug-non-zts-$PHP_API_VER/$1.so" >> /www/server/php-$PHP_VER/etc/php.ini
+}
 
 function add_redis {
     cd $SRC_DIR
@@ -15,6 +24,22 @@ function add_redis {
     ./configure --with-php-config=/www/server/php/bin/php-config
     make
     make install
+    
+    echo_ini redis
+}
+
+function add_mcrypt {
+    cd $SRC_DIR
+    wget https://pecl.php.net/get/mcrypt-1.0.1.tgz -O phpmcrypt.tar.gz
+    mkdir phpmcrypt
+    tar -zxvf phpmcrypt.tar.gz -C phpmcrypt --strip-components=1
+    cd phpmcrypt
+    phpize
+    ./configure --with-php-config=/www/server/php/bin/php-config
+    make
+    make install
+    
+    echo_ini mcrypt
 }
 
 add_$1
