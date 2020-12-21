fd0ba14cecb440ddb9f037716e7891b4a3d9cef4
Tank <tank@gmail.com>
2020-12-21 18:08:56 +0800

    fix

--- php.centos.8.sh
+++ php.centos.8.sh
@@ -237,8 +237,8 @@ function install_common {
     [ -f $COMMON_LOCK ] && return
     # iptables-services for Centos 7 and Centos 8
     yum install -y sudo wget gcc gcc-c++ make sudo autoconf libtool-ltdl-devel gd-devel \
-        freetype-devel libxml2-devel libjpeg-devel libpng-devel openssl-devel libsqlite3x-devel \
-        curl-devel patch ncurses-devel bzip2 libcap-devel sysklogd diffutils \
+        freetype-devel libxml2-devel libjpeg-devel libpng-devel openssl-devel \
+        libsqlite3x-devel curl-devel patch ncurses-devel bzip2 libcap-devel diffutils \
         bison icu libicu libicu-devel net-tools psmisc vim vim-enhanced \
         zip unzip telnet tcpdump ipset lsof iptables iptables-services
     [ $? != 0 ] && error_exit "common dependency install err"
