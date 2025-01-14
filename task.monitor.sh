a976627ea24c732705a806deb9a91c1a0abdba71
Tank <tank@gmail.com>
2025-01-14 20:24:17 +0800

    fix

---
+++ task.monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PATH="/www/server/mysql/bin:/usr/local/mysql/bin:/usr/local/bin:/usr/bin:/bin"
+export PATH
+
+BUSY_COUNT=`echo $(php /www/server/task/start.php status | grep "busy" | wc -l)`
+
+if [ `echo "$BUSY_COUNT >= 1"|bc` -eq 1 ]; then
+    #php /www/server/task/start.php restart -d > /dev/null 2>&1
+else
+    echo $BUSY_COUNT
+fi
