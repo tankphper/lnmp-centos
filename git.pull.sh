915c9b8e25655a52e9da27ed910dc04d8d58dd42
Tank <tank@gmail.com>
2026-09-09 17:30:55 +0800

    fix

---
+++ git.pull.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PATH="/www/server/mysql/bin:/usr/local/mysql/bin:/usr/local/bin:/usr/bin:/bin"
+export PATH
+
+TIME=`echo $(date +%Y-%m-%d" "%H:%M:%S)`
+
+ROOT1='/www/web/frant'
+FILE1=$ROOT1/runtime/logs/pull.log
+echo '' >> $FILE1
+echo $TIME >> $FILE1
+echo $ROOT1 >> $FILE1
+cd $ROOT1 && git pull >> $FILE1
